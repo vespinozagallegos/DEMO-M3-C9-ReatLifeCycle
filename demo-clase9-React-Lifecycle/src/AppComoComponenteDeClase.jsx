8f123140fe82ba193433a8a9a4cf545e7e9969c5
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-01-04 17:17:48 -0300

    AppComoComponenteDeClase. Se muestra la logica del componente de clase aplicado a App.

---
+++ demo-clase9-React-Lifecycle/src/AppComoComponenteDeClase.jsx
@@ -0,0 +1,23 @@
+// import React from 'react'; //forma 1
+import { Component } from 'react'; //forma 2
+import styles from './App.module.css'
+console.log(styles);
+
+
+//Al crear el comp de clase se le indica que es un comp. de clase
+//con class y extends
+//FORMA 1 ->
+//class AppComoComponenteDeClase extends React.Component {
+
+//Forma 2
+class AppComoComponenteDeClase extends Component {
+
+  return (
+    <div>
+
+    </div>
+    
+  );
+}
+
+export default AppComoComponenteDeClase;
