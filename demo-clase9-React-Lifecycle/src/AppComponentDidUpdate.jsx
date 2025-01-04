01610da1fb028c80ecea77a878d894fe3cac67af
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-01-04 18:40:30 -0300

    Se limpian los coments innecesarios

--- demo-clase9-React-Lifecycle/src/AppComponentDidUpdate.jsx
+++ demo-clase9-React-Lifecycle/src/AppComponentDidUpdate.jsx
@@ -3,13 +3,6 @@ import { Component } from 'react'; //forma 2
 import styles from './App.module.css'
 console.log(styles);
 
-
-//Al crear el comp de clase se le indica que es un comp. de clase
-//con class y extends
-//FORMA 1 ->
-//class AppComoComponenteDeClase extends React.Component {
-
-
 const user = "Víctor";
 
 //Forma 2
@@ -23,35 +16,29 @@ class AppComponentDidMount2 extends Component {
             session: false,
             info: "No se ha cargado la info"
         }
-        console.log("Ejecutando el constructor");      //! PRIMERO se ejecuta el constructor 
+        console.log("Ejecutando el constructor");
     }
 
-    //*Para ver que sucede cuando SE MONTA el COMPONENTE
-    // Permite ejecutar ciertas cosas al momento en que se carga el componente en pantalla
     //Se ejecuta UNA SOLA VEZ, cuando se monta el COMPONENTE (primera vez en pantalla)
     //*Es útil para mostrar algo "por mientras", por ej., un loading mientras se recarga lo demás.
     componentDidMount() {
         //Para saber en que momento se van ejecutando las partes de este comp.
-        console.log("Ejecutando el componenteDidMount");        //! CUARTO, el componenteDidMount
-
-        //Sólo para ver como funciona esto
+        console.log("Ejecutando el componenteDidMount");        
+        
         // setTimeout ejecuta lo que esta dentro a los 3 segundos (3000 del final) luego de ejecutarse componentDidMount()
-        setTimeout(() => {                                      //! QUINTO. Esto se ejecuta por segunda vez, 
-            //*Actualiza el estado                              //! es una actualización y el render (2° y 3°) tambien irá por segunda vez
+        setTimeout(() => {
+            //*Actualiza el estado
             this.setState({
                 session: true,
                 info: `Bienvenido ${user}`
             })
         }, 3000)
-        
-
     }
 
-    //no se puede utilizar solo return, se debe usar el metodo render()
     render() {
         //logs para ver que va ocurriendo en todo este flujo
-        console.log("Ejecutando el render");        //! SEGUNDO se ejecuta el RENDER
-        console.log(this.state.info);               //! TERCERO sigue naturalmente ESTE
+        console.log("Ejecutando el render");
+        console.log(this.state.info);
         return (
             <div>
                 <h1>Componente de Clase App</h1>
