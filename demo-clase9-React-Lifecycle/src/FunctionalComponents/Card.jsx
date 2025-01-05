5038394fc72283030876fbe61cfa4f5b961638fd
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-01-05 01:01:23 -0300

    Se agregan las propiedades name y house al return mas un boton

--- demo-clase9-React-Lifecycle/src/FunctionalComponents/Card.jsx
+++ demo-clase9-React-Lifecycle/src/FunctionalComponents/Card.jsx
@@ -1,7 +1,13 @@
 //la props es el objeto character que se desesctructura
 const Card = ({ character }) => {
     const {name, house} = character;
+    
     return (
+        <div>
+            <h2>{name}</h2>
+            <h3>{house}</h3>
+            <button>Ver Detalle</button>
+        </div>
 
     );
 }
