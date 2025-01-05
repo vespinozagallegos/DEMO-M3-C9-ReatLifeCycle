b3f7a7f4ee7a9277f7eacd50d9c01a0bda3881b2
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-01-05 02:16:49 -0300

    Actualizacion de comentarios explicativos, check

--- demo-clase9-React-Lifecycle/src/FunctionalComponents/Card.jsx
+++ demo-clase9-React-Lifecycle/src/FunctionalComponents/Card.jsx
@@ -1,5 +1,6 @@
-//la props es el objeto character que se desesctructura
+//las props son el objeto character que se desesctructura y la función handleOnClick (esto viene de la AppUseEffect)
 const Card = ({ character, handleOnClick }) => {
+    //se desestructuran sólo los elementos a utilizar de character
     const {name, house, id} = character;
     
     return (
@@ -7,7 +8,7 @@ const Card = ({ character, handleOnClick }) => {
             <h2>{name}</h2>
             <h3>{house}</h3>
             {/* Se agrega la funcion del boton que viene de la props como handleOnClick */}
-            {/* Y como el botón muestra el detalle de un personaje, se trae el id para usarlo en handleOnClick(id) */}
+            {/* Y como el botón debe mostrar el detalle de un personaje, se trae el id para usarlo en handleOnClick(id) */}
             <button onClick={() => handleOnClick(id)}>Ver Detalle</button>
         </div>
 
