f4d787df08e9ca63d1193c4f2656a19770bf490d
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-01-05 04:50:03 -0300

    Se dio formato

--- demo-clase9-React-Lifecycle/src/FunctionalComponents/Detail.jsx
+++ demo-clase9-React-Lifecycle/src/FunctionalComponents/Detail.jsx
@@ -24,15 +24,14 @@ const Detail = ({ id, handleOnClose }) => {
     return(
         // div con el estilo que tendra el modal
         <div className={styles.modalContainer}>
-                {/* si el objeto aun no se cargo, muestra etiqueta cargando... y si no :, 
-                muestra toda la etiqueta que queremos renderizar */}
+                {/* si el objeto aun no se cargo, muestra etiqueta cargando... y si no :, muestra toda la etiqueta que queremos renderizar */}
             {
                 !character ? <h2>Cargando...</h2> : (
                     <>
-                    {/* Se eligen las props a renderizar, serán solo 2 */}
-                    <h2 className={styles.title}>{character.name}</h2>   {/*si se llegase a romper se le agrega un ? entre character y el punto*/}
-                    <img src={character.image} className={styles.img} /> {/*se le puede agregar el alt pero nose pa que es alt={character.name}*/}
-                    <button className={styles.buttonDetail} onClick={handleOnClose}>Cerrar</button>
+                        {/* Se eligen las props a renderizar, serán solo 2 */}
+                        <h2 className={styles.title}>{character.name}</h2>   {/*si se llegase a romper se le agrega un ? entre character y el punto*/}
+                        <img src={character.image} className={styles.img} /> {/*se le puede agregar el alt pero nose pa que es alt={character.name}*/}
+                        <button className={styles.buttonDetail} onClick={handleOnClose}>Cerrar</button>
                     </>
                 )
             }
