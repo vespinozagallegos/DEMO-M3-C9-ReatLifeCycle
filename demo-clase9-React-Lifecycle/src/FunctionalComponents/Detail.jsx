0cefaba23da344a293163c01084dcc7b1478f48c
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-01-05 02:55:02 -0300

    Se implementa el MODAL con una función que lo cierre y se inicia el module de estilos

--- demo-clase9-React-Lifecycle/src/FunctionalComponents/Detail.jsx
+++ demo-clase9-React-Lifecycle/src/FunctionalComponents/Detail.jsx
@@ -1,1 +1,15 @@
 //* Este componente mostrará el detalle de un personaje como un MODAL
+import styles from './Detail.module.css'
+
+//recibe una función para cerrar el Modal
+const Detail = ({ handleOnClose }) => {
+    return(
+        // div con el estilo que tendra el modal
+        <div className={styles.modalContainer}>
+            <button onClick={handleOnClose}>Cerrar</button>
+        </div>
+
+    );
+}
+
+export default Detail;
