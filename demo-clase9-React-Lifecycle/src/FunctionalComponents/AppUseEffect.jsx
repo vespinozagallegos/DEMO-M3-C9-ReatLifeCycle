6b7654bea1521571d49df63fcb5406e78df8220a
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-01-05 01:07:46 -0300

    Se agrega el personaje en AppUseEffect para que Card renderice la info, falta llenar el estado aun para que renderice

--- demo-clase9-React-Lifecycle/src/FunctionalComponents/AppUseEffect.jsx
+++ demo-clase9-React-Lifecycle/src/FunctionalComponents/AppUseEffect.jsx
@@ -12,7 +12,7 @@ function AppUseEffect() {
         <>
             {
                 characters.map((character) => {
-                    return <Card key={character.id}/>
+                    return <Card key={character.id} character={character}/>
                 })
             }
         </>
